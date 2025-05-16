3c780aaf2e020289825c420ca010fbe946e73a13
Masuma <latifkarim@users-iMac-3.local>
2025-05-16 15:37:42 +0400

    footer add

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
         archivo: ['Archivo', 'sans-serif'],
+        expletus: ['Expletus Sans', 'sans-serif'],
       },
     },
   },
