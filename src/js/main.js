eafa9039968f12d69a26db576d0d0fce62530bd9
Masuma <latifkarim@users-iMac-3.local>
2025-05-16 11:11:11 +0400

    Application add

--- src/js/main.js
+++ src/js/main.js
@@ -10,4 +10,6 @@ loadComponent('header', '/templates/partials/header.html');
 loadComponent('hero', '/templates/partials/hero.html');
 loadComponent('news', '/templates/partials/news.html');
 loadComponent('specialties', '/templates/partials/specialties.html');
+loadComponent('application', '/templates/partials/application.html');
 loadComponent('footer', '/templates/partials/footer.html');
+
