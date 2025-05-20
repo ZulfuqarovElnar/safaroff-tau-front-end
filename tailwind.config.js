7909693155993f9d080ba3298690225de0734f5a
Masuma <latifkarim@users-iMac-3.local>
2025-05-20 09:59:27 +0400

    vercel fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
-    "./src/templates/**/*.html",
     "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
