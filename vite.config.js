66197c6244df1921218e2fb02f72ef5b9cc28087
Elnar Zulfuqarli <elnarzulfuqarli2001@gmail.com>
2025-06-15 09:51:24 +0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,27 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  root: '',
+  root: '.',
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       input: { 
-        main: resolve(__dirname, '/index.html'),
-        about: resolve(__dirname, '/pages/about.html'),
-        kompüter: resolve(__dirname, '/pages/kompüter-mühəndisliyi.html'),
-        sənaye: resolve(__dirname, '/pages/sənaye-mühəndisliyi.html'),
-        qida: resolve(__dirname, '/pages/qida-mühəndisliyi.html'),
-        shura: resolve(__dirname, '/pages/shura-uzvleri.html'),
-        xəbərlər: resolve(__dirname, '/pages/xəbərlər.html'),
-        xəbərlərDaxili: resolve(__dirname, '/pages/xəbərlər-daxili.html'),
-        elanlar: resolve(__dirname, '/pages/elanlar.html'),
-        elanlarDaxili: resolve(__dirname, '/pages/elanlar-daxili.html'),
-        hazırlıq: resolve(__dirname, '/pages/hazırlıq.html'),
-        academic: resolve(__dirname, '/pages/academic.html'),
-        404: resolve(__dirname, '/pages/404.html'),
-        search: resolve(__dirname, '/pages/search-result.html'),
-        contact: resolve(__dirname, '/pages/contact.html'),
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'pages/about.html'),
+        kompüter: resolve(__dirname, 'pages/kompüter-mühəndisliyi.html'),
+        sənaye: resolve(__dirname, 'pages/sənaye-mühəndisliyi.html'),
+        qida: resolve(__dirname, 'pages/qida-mühəndisliyi.html'),
+        shura: resolve(__dirname, 'pages/shura-uzvleri.html'),
+        xəbərlər: resolve(__dirname, 'pages/xəbərlər.html'),
+        xəbərlərDaxili: resolve(__dirname, 'pages/xəbərlər-daxili.html'),
+        elanlar: resolve(__dirname, 'pages/elanlar.html'),
+        elanlarDaxili: resolve(__dirname, 'pages/elanlar-daxili.html'),
+        hazırlıq: resolve(__dirname, 'pages/hazırlıq.html'),
+        academic: resolve(__dirname, 'pages/academic.html'),
+        404: resolve(__dirname, 'pages/404.html'),
+        search: resolve(__dirname, 'pages/search-result.html'),
+        contact: resolve(__dirname, 'pages/contact.html'),
       }
     }
   }
